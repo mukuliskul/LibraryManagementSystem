82b30dd90e47631b68d71f7fafdece866b987844
Mukul Sharma <mukulsharma.jan11@gmail.com>
2024-01-17 23:23:52 -0500

    Updated the tester LibAppMain.cpp

--- src/LibAppMain.cpp
+++ src/LibAppMain.cpp
@@ -2,57 +2,25 @@
 #include <iostream>
 #include "../include/LibApp.h"
 #include "../include/Date.h"
-#include "../include/Menu.h"
-using namespace myproject;
-void runApp(const char *filename)
+
+void runLibApp(const char *filename)
 {
-   size_t i{};
-   char ch;
-   char original[31] = "orig";
-   // creating original file name
-   while (filename[i])
-      original[4u + i] = filename[i++];
-   original[4u + i] = char(0);
-   // copying original values to data file;
-   std::ofstream outFile(filename);
-   std::ifstream inFile(original);
-   while (inFile.get(ch))
+   char ch{};
+   myproject::LibApp theApp(filename);
+   theApp.run();
+   std::ifstream file(filename);
+   while (file)
    {
-      outFile << ch;
+      if (file.get(ch))
+         std::cout << ch;
    }
-   outFile.close();
-   inFile.close();
-   // setting test values
+}
+int main()
+{
    myproject::sdds_day = 10;
    myproject::sdds_mon = 8;
    myproject::sdds_year = 2023;
    myproject::sdds_test = true;
-   myproject::LibApp app(filename);
-   app.run();
-   std::cout << "Content of " << filename << "\n=========>\n";
-   inFile.open(filename);
-   while (inFile.get(ch))
-      std::cout << ch;
-   std::cout << "<=========\n";
-}
-int main()
-{
-   Menu outFile("Select Data File");
-   outFile << "../data/LibRecsSmall.txt"
-           << "../data/LibRecs.txt";
-   switch (outFile.run())
-   {
-   case 1:
-      std::cout << "Test started using small data: " << std::endl;
-      runApp("../data/LibRecsSmall.txt");
-      break;
-   case 2:
-      std::cout << "Test started using big data: " << std::endl;
-      runApp("../data/LibRecs.txt");
-      break;
-   default:
-      std::cout << "Aborted by user! " << std::endl;
-      break;
-   }
+   runLibApp("data/LibRecs.txt");
    return 0;
 }
